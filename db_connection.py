f434c2e640de8b535401e56555cf9452aec5dff2
Olaf Tomalka <olaf.tomalka@gmail.com>
2016-04-09 12:08:51 +0200

    Added database connection API

---
+++ db_connection.py
@@ -0,0 +1,21 @@
+class DBWaypoint:
+    def __init__(self, node_id, name, connection_ids):
+        self.node_id = node_id
+        self.name = name
+        self.connection_ids = connection_ids
+
+class Database:
+    def __init__(self):
+        pass
+
+    def waypoint_by_name(self, name):
+        pass
+
+    def waypoint_by_id(self, node_id):
+        pass
+
+    def set_device_waypoint(self, waypoint):
+        pass
+
+    def get_device_waypoint(self):
+        pass
